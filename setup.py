366ec363fd17e76327958679e24e7ab4d8934890
Ben Cardy <ben.cardy@ocado.com>
2015-01-16 12:14:04 +0000

    Bumped version in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-  name = 'olours',
+  name = 'colours',
   packages = ['colours'],
-  version = '0.1',
+  version = '0.2',
   description = 'Easy CLI colours for Python applications',
   author = 'Ben Cardy',
   author_email = 'benbacardi@gmail.com',
   url = 'https://github.com/benbacardi/colours',
-  download_url = 'https://github.com/benbacardi/colours/tarball/0.1',
+  download_url = 'https://github.com/benbacardi/colours/tarball/0.2',
   classifiers = [],
 )
